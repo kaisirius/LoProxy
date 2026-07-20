e55c6c28ad3ab819d83bcd8bba2113be62c91aa9
Garvit Khurana <garvitkhurana47@gmail.com>
2026-07-21 01:39:05 +0530

    feat: Observer pattern for epoll engine add socket, remove socket, modify socket, fetch ready sockets

--- include/engine/EpollEngine.hpp
+++ include/engine/EpollEngine.hpp
@@ -2,17 +2,26 @@
 #include <sys/epoll.h>
 #include <memory>
 #include <mutex>
+#include <set>
+#include <vector>
 
 class EpollEngine {
     private:
         static std::unique_ptr<EpollEngine> epollEngInstance;
         static std::mutex mtx;
         int epollEngFD;
+        uint32_t defaultEvents;
+        std::set<int> registeredFDs;
         EpollEngine();
 
     public:
         EpollEngine(const EpollEngine& EpollEngine) = delete;
         EpollEngine& operator = (const EpollEngine& EpollEngine) = delete;
         static EpollEngine* getInstance();
+        epoll_event createTemplateEventStruct(int fileDescriptor);
+        void addObserver(const int fileDescriptor);
+        void removeObserver(const int fileDescriptor);
+        void modifyObserver(const int fileDescriptor,const uint32_t events);
+        std::vector<epoll_event> fetchReadySockets(int timeoutMs);
         ~EpollEngine();
 };
--- src/engine/EpollEngine.cpp
+++ src/engine/EpollEngine.cpp
@@ -1,11 +1,16 @@
 #include <engine/EpollEngine.hpp>
 #include <iostream> 
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
 
 std::unique_ptr<EpollEngine> EpollEngine::epollEngInstance = nullptr;
 std::mutex EpollEngine::mtx;
 
 EpollEngine::EpollEngine() {
     epollEngFD = epoll_create1(0);
+    defaultEvents = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLHUP;
 }
 
 
@@ -19,7 +24,93 @@ EpollEngine* EpollEngine::getInstance() {
     return epollEngInstance.get();
 }
 
+epoll_event EpollEngine::createTemplateEventStruct(int fileDescriptor) {
+
+    epoll_event templateEvent = epoll_event();
+    templateEvent.events = defaultEvents;
+    templateEvent.data.fd = fileDescriptor;
+
+    return templateEvent;
+}
+
+void EpollEngine::addObserver(const int fileDescriptor) {
+
+    if(registeredFDs.find(fileDescriptor) != registeredFDs.end()) {
+        std::cout << "[WARN]: Socekt " << fileDescriptor << " " << "already exists in epoll engine." << "\n";
+        return;
+    }
+
+    if(fileDescriptor == epollEngFD) {
+        std::cout << "[WARN]: Socekt " << fileDescriptor << " " << "is same as epoll engine's FD." << "\n";
+        return;
+    }
+    
+    epoll_event event = createTemplateEventStruct(fileDescriptor);
+    int flag = epoll_ctl(epollEngFD, EPOLL_CTL_ADD, fileDescriptor, &event);
+    if(flag == -1) {
+        std::cerr << "[ERROR]: " << "Cannot register socket to epoll engine." << "\n";
+        std::string err = strerror(errno);
+        throw std::runtime_error(err);
+    } else {
+        registeredFDs.insert(fileDescriptor);
+        std::cout << "[LOG]: Socekt " << fileDescriptor << " " << "successfully added to epoll engine." << "\n";
+    }
+}
+
+void EpollEngine::removeObserver(const int fileDescriptor) {
+
+    if(registeredFDs.find(fileDescriptor) == registeredFDs.end()) {
+        std::cout << "[WARN]: Socekt " << fileDescriptor << " " << "does not exist in epoll engine. Nothing to delete." << "\n";
+        return;
+    }
+
+    int flag = epoll_ctl(epollEngFD, EPOLL_CTL_DEL, fileDescriptor, nullptr);
+    if(flag == -1) {
+        std::cerr << "[ERROR]: " << "Cannot de-register socket from epoll engine." << "\n";
+        std::string err = strerror(errno);
+        throw std::runtime_error(err);
+    } else {
+        std::cout << "[LOG]: Socekt " << fileDescriptor << " " << "successfully removed from epoll engine." << "\n";
+    }
+}
+
+void EpollEngine::modifyObserver(const int fileDescriptor, const uint32_t events) {
+
+    if(registeredFDs.find(fileDescriptor) == registeredFDs.end()) {
+        std::cout << "[WARN]: Socekt " << fileDescriptor << " " << "does not exist in epoll engine. Nothing to modify." << "\n";
+        return;
+    }
+
+    epoll_event updatedEvents = createTemplateEventStruct(fileDescriptor);
+    updatedEvents.events = events;
+
+    int flag = epoll_ctl(epollEngFD, EPOLL_CTL_MOD, fileDescriptor, &updatedEvents);
+    if(flag == -1) {
+        std::cerr << "[ERROR]: " << "Cannot modify socket from epoll engine." << "\n";
+        std::string err = strerror(errno);
+        throw std::runtime_error(err);
+    } else {
+        std::cout << "[LOG]: Socekt " << fileDescriptor << " " << "successfully modified in epoll engine." << "\n";
+    }
+}
+
+std::vector<epoll_event> EpollEngine::fetchReadySockets(int timeoutMs) {
+    if(timeoutMs < 0) {
+        // -1 will lead to infinite loop
+        std::cout << "Invalid time in millisecond as an argument." << "\n";
+        return {};
+    }
+    // -1 will be used as flag to check how many ready sockets we received 
+    // other work around is to return pair<vector, int>  = (readySockets, numberOfReadySockets)
+    std::vector<epoll_event> readySockets(1000, createTemplateEventStruct(-1));
+
+    int numberOfReadySockets = epoll_wait(epollEngFD, &readySockets[0], 1000, timeoutMs);
+    return readySockets;
+}
+
+
 EpollEngine::~EpollEngine() {
     std::cout << "----------Stopping epoll engine----------" << "\n";
-    // delete epollEngInstance.get();
+    close(epollEngFD);
+    // unnecessary to do delete epollEngInstance.get();
 }
